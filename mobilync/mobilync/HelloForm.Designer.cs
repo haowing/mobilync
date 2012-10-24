4838f80a112e9248b1074c2e75b86e54859ef3a8
unknown <dingh@P-112-100-00472.posten.local>
2012-10-24 12:51:05 +0200

    first commit with project files

---
+++ mobilync/mobilync/HelloForm.Designer.cs
@@ -0,0 +1,48 @@
+﻿using System;   
+using System.Data;
+using System.ComponentModel;
+using System.Windows.Forms;
+namespace mobilync
+{
+    partial class HelloForm
+    {
+        Button btnTest;
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "Hello Posten Form";
+            this.Text = "Hello Posten!";
+
+            btnTest = new Button();
+            
+        }
+
+        #endregion
+
+        private void layoutComponents() {
+            btnTest.Name="testButton";
+            btnTest.Location = new System.Drawing.Point(120,48);
+            btnTest.Size = new System.Drawing.Size(160,32);
+            btnTest.TabIndex = 0;
+            btnTest.Text = "Click";
+
+            this.Controls.Add(btnTest);// this.Controls.AddRange(new System.Windows.Forms.Control[] { this.btnTest });
+
+        }
+
+        private void configureComponents() {
+            btnTest.Click += new System.EventHandler(this.btnTest); 
+
+        }
+    }
+
+
+}
---
+++ mobilync/mobilync/HelloForm.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace mobilync
+{
+    public partial class HelloForm : Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public HelloForm()
+        {
+            InitializeComponent(); //createComponents();
+            layoutComponents();
+            configureComponents();
+            //addBehaviors();
+
+
+        }
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        static void Main() {
+            Application.Run(new HelloForm());
+        }
+    }
+
+
+}
